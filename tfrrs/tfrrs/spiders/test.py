03906e4929303ff236185838edba589d2d9c550d
Megts <78006015+Megts@users.noreply.github.com>
2021-01-31 13:34:19 -0600

    runner spider now gets NCAA college names and their tfrrs href and writes it to a csv file where it goes name,href and then a newline

--- tfrrs/tfrrs/spiders/test.py
+++ tfrrs/tfrrs/spiders/test.py
@@ -7,13 +7,17 @@ class TFRRSspider(scrapy.Spider):
     name = "runner"
 
     def start_requests(self):
-        urls = ["https://www.tfrrs.org/athletes/6592815/Wartburg/Joe_Freiburger"]
+        urls = ["https://www.tfrrs.org/leagues/48.html"]
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
 
 
     def parse(self, response):
-        filename = response.url.split("/")[-2] + '.' + response.url.split("/")[-2] + '.html'
-        with open(filename, 'wb') as f:
-            f.write(response.body)
+        filename = 'NCAA_teams.csv'
+        with open(filename, 'w') as f:
+            teamName = response.xpath('//div[@class="col-lg-4"]/table/tbody/tr/td/a/text()').getall()
+            teamLink = response.xpath('//div[@class="col-lg-4"]/table/tbody/tr/td/a/@href').getall()
+            for i in range(len(teamName)):
+#                print(teamName[i] + "," + teamLink[i])
+                f.write(teamName[i] + "," + teamLink[i] +'\n')
         self.log('Saved file as %s' % filename)
