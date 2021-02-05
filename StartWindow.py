931bc09033d387ca23a55f99242e7dc85d075a0d
Nick Meling <78007228+nmeling@users.noreply.github.com>
2021-02-05 11:09:06 -0600

    Updated windows
    SelectionWindow.py is now gone it should be SelectionWindow1.py and SelectionWindow2.py as well as the same StartWindow.py

--- StartWindow.py
+++ StartWindow.py
@@ -2,29 +2,32 @@
 #By: Nick Meling and Matt Egts
 
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QComboBox, QPushButton, QWidget, QDesktopWidget
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+
 
 def window():
 #Window set up
     app=QApplication(sys.argv)
     win=QWidget()
-    win.setGeometry(400,150,700,700)
+    win.setGeometry(400,150,700,500)
     win.setWindowTitle("TFRRS Visualizer")
     win.setStyleSheet("background-color: gray;")
+    
 #Text description 1
     label= QLabel(win)
-    label.setText("This program will allow you to visualize athlete stats in Cross Country or Track and Field.")
-    label.move(100,50)
-    label.setStyleSheet("background-color: orange;")
-#Text description 2
-    label= QLabel(win)
-    label.setText("You will be prompted to select NCAA Division, Gender, College, Sport, and Event.")
-    label.move(100,66)
-    label.setStyleSheet("background-color: orange;")
+    label.setText("This program will allow you to visualize athlete statistics in Cross Country or Track and Field. You will be prompted to select NCAA Division, Gender, College, Sport, and Event.")
+    label.setWordWrap(True)
+    label.setAlignment(Qt.AlignCenter)
+    label.move(200,200)
+    
+    label.setStyleSheet('QLabel {color: Orange}')
+    
 #Button to continue to next window
     button= QPushButton(win)
     button.setText("Click here to continue")
-    button.move(300,350)
+    button.move(285,350)
     button.setStyleSheet("background-color: orange;")
   
     win.show()
