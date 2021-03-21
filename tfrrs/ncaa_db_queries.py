cebd9a21078324f8df876ec5b46ffa845b5f6ea5
Megts <78006015+Megts@users.noreply.github.com>
2021-03-21 14:28:10 -0500

    changing datetime to datetime 64 units

--- tfrrs/ncaa_db_queries.py
+++ tfrrs/ncaa_db_queries.py
@@ -2,7 +2,7 @@
 # queries for getting information from the database
 
 import sqlite3
-from datetime import datetime
+from numpy import datetime64
 
 class DB:
 
@@ -34,7 +34,8 @@ class DB:
         self._start_connection()
         teams = self.curr.execute("""SELECT name, college_id
                                     FROM Colleges
-                                    WHERE division = ? AND gender = ?""",
+                                    WHERE division = ? AND gender = ?
+                                    ORDER BY name""",
                                     (division, gender))
         teams = teams.fetchall()
         return teams
@@ -43,7 +44,8 @@ class DB:
         self._start_connection()
         cmd = f"""SELECT (first_name || ' ' || last_name) as name, athlete_id
                                 FROM Athletes
-                                WHERE college_id = {team_id!r}"""
+                                WHERE college_id = {team_id!r}
+                                ORDER BY name"""
         roster = self.curr.execute(cmd)
         roster = roster.fetchall()
         self._close_connection()
@@ -58,7 +60,8 @@ class DB:
                                             FROM Performances as p) as perf
                                 ON a.athlete_id = perf.athlete_id
                                 WHERE college_id = {team_id!r} AND a.athlete_id NOT IN {ath_ids}
-                                AND perf.season = {season!r} AND perf.event_name = {event!r}"""
+                                AND perf.season = {season!r} AND perf.event_name = {event!r}
+                                ORDER BY name"""
         roster = self.curr.execute(cmd)
         roster = roster.fetchall()
         self._close_connection()
@@ -66,11 +69,11 @@ class DB:
 
     def get_ahtlete_name(self, athlete_id):
         self._start_connection()
-        name = self.curr.execute("""SELECT first_name, last_name
+        name = self.curr.execute("""SELECT (first_name || ' ' || last_name) as name
                                     FROM Athletes WHERE athlete_id = ?""",
                                     (athlete_id,)).fetchall()
         self._close_connection()
-        return name[0][0] + ' ' + name[0][1]
+        return name[0][0]
 
     def get_athlete_seasons(self, athlete_id):
         self._start_connection()
@@ -92,7 +95,7 @@ class DB:
             cmd += f"""SELECT season FROM Performances WHERE athlete_id = {athlete_ids[i]}"""
             if i < len(athlete_ids)-1:
                 cmd += """ INTERSECT """
-        cmd += """) as u"""
+        cmd += """) as u ORDER BY u.season"""
         seasons = self.curr.execute(cmd)
         seasons = seasons.fetchall()
         self._close_connection()
@@ -140,34 +143,45 @@ class DB:
                 marks.append(meters)
                 wind2.append(windL2)
                 wind4.append(windL4)
-                dates.append(datetime(year,month+1,day))
+                dates.append(self._date_to_dt(year,month,day))
         else:
             units = 'Time'
             for min, seconds, windL2, windL4, day, month, year in performances:
-                if seconds is not None:
-                    sec = int(seconds)
-                    micro = int((seconds-sec) * 10**6)
-                    if min is None:
-                        min = 0
-                    marks.append(datetime(3,3,15,minute=min,second=sec,microsecond=micro))
+                if seconds > 100:
+                    units = "Points"
+                    marks.append(seconds)
+                    wind2.append(windL2)
+                    wind4.append(windL4)
+                    dates.append(self._date_to_dt(year,month,day))
                 else:
-                    marks.append(None)
-                wind2.append(windL2)
-                wind4.append(windL4)
-                dates.append(datetime(year,month + 1,day))
+                    if seconds is not None:
+                        sec = int(seconds)
+                        hundreth = int((seconds-sec) * 100)
+                        if min is None:
+                            min = 0
+                        marks.append(self._time_to_dt(min,sec,hundreth))
+                    else:
+                        marks.append(None)
+                    wind2.append(windL2)
+                    wind4.append(windL4)
+                    dates.append(self._date_to_dt(year,month,day))
         return marks,dates,units,wind2,wind4
+
+    def _get_units(self, event_name):
+        self._start_connection()
+        unit = self.curr.execute("""SELECT time_or_dist FROM Performances
+                                    WHERE event_name = ?""", (event_name,)).fetchall()
+        return unit[0][0]
 
     def _tuplist_to_list(self, tuplist):
         return [item for tup in tuplist for item in tup]
 
-    def to_seconds(self,min,seconds):
-        if seconds is not None:
-            if min is not None:
-                    return min*60 + seconds
-            return seconds
-        return None
+    def _time_to_dt(self, min,sec,hundreth):
+        min = ('0' + str(min))[-2:]
+        return datetime64(f'0001-01-01T00:{self._2digs(min)}:{sec}.{hundreth}')
 
-    def to_minutes(self,min,seconds):
-        if min is not None:
-            return min + seconds/60
-        return None
+    def _date_to_dt(self,year,month,day):
+        return datetime64(f'{year}-{self._2digs(month+1)}-{self._2digs(day)}')
+
+    def _2digs(self, num):
+        return ('0' + str(num))[-2:]
