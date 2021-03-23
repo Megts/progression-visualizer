f52adc7313e16ab2945cfc756582647c97602a3e
Megts <78006015+Megts@users.noreply.github.com>
2021-03-23 11:10:12 -0500

    Plot now only shows 1 season length instead of whole career

--- tfrrs/ncaa_db_queries.py
+++ tfrrs/ncaa_db_queries.py
@@ -119,7 +119,7 @@ class DB:
         performances = self.curr.execute("""SELECT min,sec_or_meters,wind_legal2,wind_legal4,day,month,year
                                           FROM Performances
                                           WHERE athlete_id = ? AND event_name = ? AND season = ?
-                                          ORDER BY year, month DESC, day DESC""", (
+                                          ORDER BY year, month, day""", (
                                                 athlete_id,
                                                 event_name,
                                                 season
@@ -145,67 +145,76 @@ class DB:
         if units == 'dist':
             units = 'Meters'
             for min, meters, windL2, windL4, day, month, year in performances:
+                if season=='Indoor' and (year>season_year or (year==season_year and month>3)):
+                    completed_seasons.append([marks,wind2,wind4,dates,season_year])
+                    season_year = year
+                    if month == 11 and season == 'Indoor':
+                        season_year += 1
+                    marks = []
+                    dates = []
+                    wind2 = []
+                    wind4 = []
+                else:
+                    if year > season_year:
+                        completed_seasons.append([marks,wind2,wind4,dates, season_year])
+                        season_year = year
+                        marks = []
+                        dates = []
+                        wind2 = []
+                        wind4 = []
                 marks.append(meters)
                 wind2.append(windL2)
                 wind4.append(windL4)
-                dates.append(self._date_to_std_dt(year,month,day, year1))
-                if season == 'Indoor':
-                    if year >= season_year and month > 3:
-                        season_year = year
-                        if month == 11:
-                            season_year += 1
-                        completed_seasons.append([marks,wind2,wind4,dates])
-                        marks = []
-                        dates = []
-                        wind2 = []
-                        wind4 = []
-                else:
-                    if year > season_year:
-                        season_year = year
-                        completed_seasons.append([marks,wind2,wind4,dates])
-                        marks = []
-                        dates = []
-                        wind2 = []
-                        wind4 = []
+                dates.append(self._date_to_2000_dt(year,month,day, season_year))
         else:
             units = 'Time'
             for min, seconds, windL2, windL4, day, month, year in performances:
+
+                if season=='XC' and year>season_year and month>3:
+                    completed_seasons.append([marks,wind2,wind4,dates,season_year])
+                    season_year = year
+                    marks = []
+                    dates = []
+                    wind2 = []
+                    wind4 = []
+                elif season=='Indoor' and (year>season_year or (year==season_year and month>3)):
+                    completed_seasons.append([marks,wind2,wind4,dates,season_year])
+                    season_year = year
+                    if month == 11 and season == 'Indoor':
+                        season_year += 1
+                    marks = []
+                    dates = []
+                    wind2 = []
+                    wind4 = []
+                else:
+                    if year > season_year:
+                        completed_seasons.append([marks,wind2,wind4,dates,season_year])
+                        season_year = year
+                        marks = []
+                        dates = []
+                        wind2 = []
+                        wind4 = []
+
                 if seconds > 100:
                     units = "Points"
                     marks.append(seconds)
                     wind2.append(windL2)
                     wind4.append(windL4)
-                    dates.append(self._date_to_std_dt(year,month,day, year1))
+                    dates.append(self._date_to_2000_dt(year,month,day, season_year))
                 else:
                     if seconds is not None:
                         sec = int(seconds)
                         hundreth = int((seconds-sec) * 100)
                         if min is None:
                             min = 0
-                        marks.append(self._date_to_std_dt(year,month,day, year1))
+                        marks.append(self._time_to_dt(min,sec,hundreth))
                     else:
                         marks.append(None)
                     wind2.append(windL2)
                     wind4.append(windL4)
-                    dates.append(self._date_to_std_dt(year,month,day, year1))
-                if season in ['Indoor', 'XC']:
-                    if year >= season_year and month > 3:
-                        season_year = year
-                        if month == 11 and season == 'Indoor':
-                            season_year += 1
-                        completed_seasons.append([marks,wind2,wind4,dates])
-                        marks = []
-                        dates = []
-                        wind2 = []
-                        wind4 = []
-                else:
-                    if year >= season_year and month:
-                        season_year = year
-                        completed_seasons.append([marks,wind2,wind4,dates])
-                        marks = []
-                        dates = []
-                        wind2 = []
-                        wind4 = []
+                    dates.append(self._date_to_2000_dt(year,month,day, season_year))
+
+        completed_seasons.append([marks,wind2,wind4,dates, season_year])
         return completed_seasons, units
 
     def _get_units(self, event_name):
@@ -219,27 +228,16 @@ class DB:
 
     def _time_to_dt(self, min,sec,hundreth):
         min = ('0' + str(min))[-2:]
-        return datetime64(f'0001-01-01T00:{self._2digs(min)}:{self._2digs(sec)}.{hundreth}')
+        return datetime64(f'2000-01-01T00:{self._2digs(min)}:{self._2digs(sec)}.{hundreth}')
 
-    def _date_to_std_dt(self,year,month,day, year1):
+    def _date_to_2000_dt(self,year,month,day, season_year):
         dt = datetime64(f'{year}-{self._2digs(month+1)}-{self._2digs(day)}')
-        if year < year1:
-            return dt
-        td = datetime64(f'{year1}-01-01')-datetime64(f'{year}-01-01')
+        td = datetime64(f'{season_year}-01-01')-datetime64('2000-01-01')
         return dt - td
+
     def _2digs(self, num):
         return ('0' + str(num))[-2:]
-    def to_seconds(self,min,seconds):
-        if seconds is not None:
-            if min is not None:
-                    return min*60 + seconds
-            return seconds
-        return None
 
-    def to_minutes(self,min,seconds):
-        if min is not None:
-            return min + seconds/60
-        return None
 #Athlete 1 Personal Record (We need min for sec and Max for meters how do we fix this?)
     def get_athlete_pr(self,athlete_id, event_name):
         self._start_connection()
